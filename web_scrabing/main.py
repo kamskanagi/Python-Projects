eb2de0b3bed0ca5b43c2c5e472e588558e3a8326
karamo kanagi <karamo.kanagi@china.nttdata.com>
2023-06-30 11:05:47 +0800

    updated web scraping

--- web_scrabing/main.py
+++ web_scrabing/main.py
@@ -1,15 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
-header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-}
+mystocks = ['VAST.L', ' ICON.L', 'PREM.L', 'BZT.L', 'ASPL.L']
+stockdata = []
 
-url = 'https://uk.finance.yahoo.com/quote/ASPL.L'
-res = requests.get(url, headers=header)
-# print(res.status_code)
+def getData(symbol):
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    url = f'https://uk.finance.yahoo.com/quote/{symbol}'
+    res = requests.get(url, headers=header)
+    # print(res.status_code)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    stock= {
+        'symbol': symbol,
+        'price':soup.find('div',  {'class':'D(ib) Mend(20px)'}).find_all('span')[0].text,
+        'change':soup.find('span',  {'class':'D(ib) Mend(20px)'}).find_all('span')[1].text
+    }
+    return stock
 
-soup = BeautifulSoup(res.text, 'html.parser')
+#print(getData('ASPL.L'))
+for item in mystocks:
+    stockdata.append(getData(item))
+    print("Getting:", item)
 
-
-# price =  soup. find('span',  {' '})
+with open('stockdata.json', 'w') as f:
+    json.dump(stockdata, f)
